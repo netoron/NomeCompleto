1c8fe145320ba06c1b1963cf58c9f9b382a99c68
netoron <61334943+netoron@users.noreply.github.com>
2020-05-15 23:52:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu Primeiro Nome");
+            String primeiro = Console.ReadLine();
+            Console.WriteLine("Digite seu Segundo Nome");
+            String Segundo = Console.ReadLine();
+            Console.WriteLine("Nome completo"); 
+            string Dois = primeiro + " " + Segundo;
+            Console.WriteLine(Dois);
+            Console.WriteLine("Nome de catálogo");
+            string tres = Segundo + " " + primeiro;
+            Console.WriteLine(tres);
+        }
+    }
+}
